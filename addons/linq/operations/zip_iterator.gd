5b670a045ba08290ae3a9a11ba52f0f5a007e9a3
MatthijsMud <MatthijsMud@users.noreply.github.com>
2025-04-24 21:29:44 +0200

    Add `Enumerable.Zip` (#8)
    This pull-request adds support for the `zip(…)` operator. It accepts
    another object which can be turned into an `Iterator` by calling
    `Iterator.from(…)`. Each element in the result source will be an array
    with two element: `[source[i], other[i]]`.
    
    It generates elements as long as both sources generate elements.
    
    closes #7

---
+++ addons/linq/operations/zip_iterator.gd
@@ -0,0 +1,24 @@
+class_name ZipIterator extends ChainedIterator
+
+var _other: Iterator;
+
+func _init(source: Iterator, other: Iterator) -> void:
+	super(source);
+	_other = other;
+
+func _iter_init(iter: Array) -> bool:
+	var state := State.new();
+	iter[0] = state;
+	return _source._iter_init(state.source_state) and _other._iter_init(state.other_state);
+	
+func _iter_next(iter: Array) -> bool:
+	var state := iter[0] as State;
+	return _source._iter_next(state.source_state) and _other._iter_next(state.other_state);
+	
+func _iter_get(iter: Variant) -> Variant:
+	var state := iter[0] as State;
+	return [_source._iter_get(state.source_state), _other._iter_get(state.other_state)];
+
+class State extends RefCounted:
+	var source_state := [null];
+	var other_state := [null];
