2f2acdf91bc4137c2d0f1b4c18a528643c6db7b6
Matthijs Mud <matthijsmud@gmail.com>
2025-05-23 15:06:31 +0200

    Specialize `ArrayIterator._default`.
    `ArrayIterator` knows the type of the underlying array being iterated, and can thus provide meaningful default values.

--- addons/linq/sources/array_iterator.gd
+++ addons/linq/sources/array_iterator.gd
@@ -1,8 +1,8 @@
 class_name ArrayIterator extends Iterator
 
-var _source: Array;
+var _source: Variant;
 
-func _init(source: Array) -> void:
+func _init(source: Variant) -> void:
 	_source = source;
 
 func _iter_init(iter: Array) -> bool:
@@ -15,3 +15,21 @@ func _iter_next(iter: Array) -> bool:
 
 func _iter_get(iter: Variant) -> Variant:
 	return _source[iter];
+
+func _default() -> Variant:
+	match typeof(_source):
+		TYPE_ARRAY:
+			return type_convert(null, (_source as Array).get_typed_builtin());
+		TYPE_PACKED_BYTE_ARRAY: return type_convert(null, TYPE_INT); # GdScript does not have a [byte] type.
+		TYPE_PACKED_COLOR_ARRAY: return type_convert(null, TYPE_COLOR);
+		TYPE_PACKED_FLOAT32_ARRAY: return type_convert(null, TYPE_FLOAT);
+		TYPE_PACKED_FLOAT64_ARRAY: return type_convert(null, TYPE_FLOAT);
+		TYPE_PACKED_INT32_ARRAY: return type_convert(null, TYPE_INT);
+		TYPE_PACKED_INT64_ARRAY: return type_convert(null, TYPE_INT);
+		TYPE_PACKED_VECTOR2_ARRAY: return type_convert(null, TYPE_VECTOR2);
+		TYPE_PACKED_VECTOR3_ARRAY: return type_convert(null, TYPE_VECTOR3);
+		TYPE_PACKED_VECTOR4_ARRAY: return type_convert(null, TYPE_VECTOR4);
+		TYPE_PACKED_STRING_ARRAY: return type_convert(null, TYPE_STRING);
+		
+		var type: push_error("[ArrayIterator] could not determine intended default for {type}".format({ "type": type_string(type) }));
+	return null;
