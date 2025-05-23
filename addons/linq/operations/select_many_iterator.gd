13413b22932073e3651af0a4efc83a1615640c41
Matthijs Mud <matthijsmud@gmail.com>
2025-05-23 15:18:35 +0200

    Replaced some default parameters with more explicit `undefined` value.

--- addons/linq/operations/select_many_iterator.gd
+++ addons/linq/operations/select_many_iterator.gd
@@ -3,7 +3,7 @@ class_name SelectManyIterator extends ChainedIterator
 var _collection_selector: Callable;
 var _result_selector: Callable;
 
-func _init(source: Iterator, collection_selector: Callable, result_selector: Callable = Callable()) -> void:
+func _init(source: Iterator, collection_selector: Callable, result_selector: Callable = UNDEFINED) -> void:
 	super(source);
 	_collection_selector = _resolve_collection_selector_overload(collection_selector);
 	_result_selector = _resolve_result_selector_overload(result_selector);
@@ -25,8 +25,12 @@ func _resolve_collection_selector_overload(collection_selector: Callable) -> Cal
 	return Callable();
 
 func _resolve_result_selector_overload(result_selector: Callable) -> Callable:
-	if not result_selector.is_valid():
+	if is_same(result_selector, UNDEFINED):
 		return _default_result_selector;
+		
+	if not result_selector.is_valid():
+		push_error("[SelectManyIterator] requires either a valid [param result_selector] or its default value.");
+		return result_selector;
 		
 	match result_selector.get_argument_count():
 		# func result_selector() -> Variant
