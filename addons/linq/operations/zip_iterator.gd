fd3ea3cae16f36c439094bf78ff5723ef3dc28b5
Matthijs Mud <matthijsmud@gmail.com>
2025-05-23 15:01:13 +0200

    Specialize `ZipIterator._default`.

--- addons/linq/operations/zip_iterator.gd
+++ addons/linq/operations/zip_iterator.gd
@@ -19,6 +19,9 @@ func _iter_get(iter: Variant) -> Variant:
 	var state := iter[0] as State;
 	return [_source._iter_get(state.source_state), _other._iter_get(state.other_state)];
 
+func _default() -> Variant:
+	return [_source._default(), _other._default()];
+
 class State extends RefCounted:
 	var source_state := [null];
 	var other_state := [null];
