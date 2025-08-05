75a733fd32876605b6024ceed02b471c05542956
Matthijs Mud <matthijsmud@gmail.com>
2025-08-05 12:39:36 +0200

    Forward the `_default()` invocation.

--- addons/linq/operations/chained_iterator.gd
+++ addons/linq/operations/chained_iterator.gd
@@ -1,8 +1,11 @@
+## "Abstract" [Iterator] that operates on the provided [param source].
+## By default it forwards all calls to its source.
 class_name ChainedIterator extends Iterator
 
 var _source: Iterator;
 
 func _init(source: Iterator) -> void:
+	if not source: push_error("[ChainedIterator] expect source to not be [null].");
 	_source = source;
 
 func _iter_init(iter: Array) -> bool:
@@ -13,3 +16,6 @@ func _iter_next(iter: Array) -> bool:
 
 func _iter_get(iter: Variant) -> Variant:
 	return _source._iter_get(iter);
+
+func _default() -> Variant:
+	return _source._default();
